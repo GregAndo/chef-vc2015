fb1356326a2703ca738330348732d116289617fe
Greg Anderson <ganderson@raywhite.com>
2016-07-05 13:22:45 +1000

    Specify version of package

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,9 +7,9 @@
 #
 
 # Microsoft Visual C++ 2015  x86 Redistributable
-default['vc2015']['x86']['productname'] = 'Microsoft Visual C++ 2015  x86 Redistributable - 10.0.30319'
+default['vc2015']['x86']['productname'] = 'Microsoft Visual C++ 2015  x86 Redistributable - 14.0.23026.0'
 default['vc2015']['x86']['url'] = 'https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x86.exe'
 
 # Microsoft Visual C++ 2015  x64 Redistributable
-default['vc2015']['x64']['productname'] = 'Microsoft Visual C++ 2015  x64 Redistributable - 10.0.30319'
+default['vc2015']['x64']['productname'] = 'Microsoft Visual C++ 2015  x64 Redistributable - 14.0.23026.0'
 default['vc2015']['x64']['url'] = 'https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe'
